0a63954ad7853258b1d765446bab071d70271daf
oroszgy <oroszgy@gmail.com>
2012-11-28 21:50:31 +0100

    Tag mapper implementation is done.

--- src/main/java/hu/ppke/itk/nlpg/purepos/model/internal/TagMapper.java
+++ src/main/java/hu/ppke/itk/nlpg/purepos/model/internal/TagMapper.java
@@ -25,14 +25,45 @@ package hu.ppke.itk.nlpg.purepos.model.internal;
 import hu.ppke.itk.nlpg.purepos.model.IMapper;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TagMapper implements IMapper<Integer> {
+	LinkedHashMap<Pattern, String> mapping;
+	private Vocabulary<String, Integer> vocabulary;
+
+	public TagMapper(Vocabulary<String, Integer> vocabulary) {
+		this.vocabulary = vocabulary;
+		mapping = new LinkedHashMap<Pattern, String>();
+		init();
+	}
+
+	private void init() {
+		add("^(.*)(\\|lat)(.*)$", "$1$3");
+	}
+
+	private void add(String regexp, String replacement) {
+		Pattern p = Pattern.compile(regexp);
+		mapping.put(p, replacement);
+	}
 
 	@Override
 	public Integer map(Integer tag) {
-		// TODO Auto-generated method stub
-		return null;
+		String tagStr = vocabulary.getWord(tag);
+		for (Pattern p : mapping.keySet()) {
+			Matcher matcher = p.matcher(tagStr);
+			if (matcher.matches()) {
+				String replacement = mapping.get(p);
+				String repTagStr = matcher.replaceAll(replacement);
+				Integer retTag = vocabulary.getIndex(repTagStr);
+				if (retTag != null)
+					return retTag;
+			}
+		}
+		return tag;
+
 	}
 
 	@Override
@@ -41,7 +72,6 @@ public class TagMapper implements IMapper<Integer> {
 		for (Integer e : elements) {
 			ret.add(map(e));
 		}
-		// TODO Auto-generated method stub
 		return ret;
 	}
 
---
+++ src/test/java/hu/ppke/itk/nlpg/purepos/model/internal/TagMapperTest.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2012 György Orosz, Attila Novák.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/
+ * 
+ * This file is part of PurePos.
+ * 
+ * PurePos is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PurePos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * Contributors:
+ *     György Orosz - initial API and implementation
+ ******************************************************************************/
+package hu.ppke.itk.nlpg.purepos.model.internal;
+
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TagMapperTest {
+
+	@Test
+	public void test() {
+		Vocabulary<String, Integer> vocabulary = new IntVocabulary<String>();
+		Integer fn = vocabulary.addElement("[FN][NOM]");
+		Integer mn = vocabulary.addElement("[MN][NOM]");
+		Integer fnlat = vocabulary.addElement("[FN|lat][NOM]");
+		Integer mnlat = vocabulary.addElement("[MN|lat][NOM]");
+		Integer ige = vocabulary.addElement("[IGE][Me3]");
+		TagMapper mapper = new TagMapper(vocabulary);
+		Assert.assertEquals(mn, mapper.map(mnlat));
+		Assert.assertEquals(fn, mapper.map(fnlat));
+		Assert.assertEquals(fn, fn);
+		Assert.assertEquals(ige, ige);
+		List<Integer> from = Arrays.asList(fn, mnlat, fnlat);
+		List<Integer> to = Arrays.asList(fn, mn, fn);
+		Assert.assertEquals(to, mapper.map(from));
+	}
+}
