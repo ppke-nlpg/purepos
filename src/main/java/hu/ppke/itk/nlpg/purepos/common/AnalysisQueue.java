ee80031218e4f0594f2af41615b9b47674e02d46
oroszgy <oroszgy@gmail.com>
2013-01-10 14:28:09 +0100

    AnalQueue is implemented.

---
+++ src/main/java/hu/ppke/itk/nlpg/purepos/common/AnalysisQueue.java
@@ -0,0 +1,128 @@
+/*******************************************************************************
+ * Copyright (c) 2012 György Orosz, Attila Novák.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/
+ * 
+ * This file is part of PurePos.
+ * 
+ * PurePos is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PurePos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * Contributors:
+ *     György Orosz - initial API and implementation
+ ******************************************************************************/
+package hu.ppke.itk.nlpg.purepos.common;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Global holder for tags and stems in a pretagged input.
+ * 
+ * @author György Orosz
+ * 
+ */
+public class AnalysisQueue {
+	// (position, (tag+stem, prob)
+	protected ArrayList<Map<String, Double>> anals;
+	// (tag, (stem, prob))
+	// protected ArrayList<Map<String, Double>> stems;
+	// if the input contains probability information
+	protected ArrayList<Boolean> useProb;
+	protected ArrayList<String> words;
+
+	protected static String alnumPat = "\\p{L}\\p{N}";
+	protected static String punctPat = "\\p{P}";
+
+	protected static String stringPat = "([" + alnumPat + punctPat + "]+)";
+	protected static String analPat = "((" + stringPat + "(\\[[" + alnumPat
+			+ "]+\\]))+(\\$\\$[0-9]+(\\.[0-9]+)?)?)";
+
+	protected static String analSplitRe = "\\|\\|";
+	protected static String weightSplitPat = "\\$\\$";
+
+	protected static Pattern analFormPat = Pattern.compile(stringPat
+			+ "\\{\\{(" + analPat + "(\\|\\|" + analPat + ")*" + ")\\}\\}");
+
+	public void init(int capacity) {
+		anals = new ArrayList<Map<String, Double>>(capacity);
+		// stems = new ArrayList<Map<String, Double>>(capacity);
+		useProb = new ArrayList<Boolean>(capacity);
+		words = new ArrayList<String>(capacity);
+		for (int i = 0; i < capacity; ++i) {
+			anals.add(null);
+			// stems.add(null);
+			useProb.add(null);
+			words.add(null);
+		}
+	}
+
+	public void addWord(String input, Integer position) {
+		Matcher m = analFormPat.matcher(input);
+		String word = m.replaceAll("$1");
+		String analsStrings = m.replaceAll("$2");
+
+		words.set(position, word);
+		anals.set(position, new HashMap<String, Double>());
+
+		List<String> analsList = Arrays.asList(analsStrings.split(analSplitRe));
+		for (String anal : analsList) {
+			int indexOfValSep = anal.indexOf("$$");
+			String lemmaTag = anal;
+			double prob = 1;
+			if (indexOfValSep > -1) {
+				useProb.set(position, true);
+				prob = Double.parseDouble(anal.substring(indexOfValSep + 2));
+				lemmaTag = anal.substring(0, indexOfValSep);
+			}
+			anals.get(position).put(lemmaTag, prob);
+
+		}
+
+	}
+
+	public boolean hasAnal(Integer position) {
+		return anals.size() > position && anals.get(position) != null;
+	}
+
+	public Map<String, Double> getAnals(Integer position) {
+		return anals.get(position);
+	}
+
+	public boolean useProbabilties(Integer position) {
+		if (useProb.size() > position)
+			return useProb.get(position) != null;
+		return false;
+
+	}
+
+	public static boolean isPreanalysed(String word) {
+		return analFormPat.matcher(word).matches();
+	}
+
+	public static String clean(String word) {
+		return analFormPat.matcher(word).replaceAll("$1");
+	}
+
+	public static String anal2tag(String anal) {
+		return anal.substring(anal.indexOf("["));
+	}
+
+	public static String anal2lemma(String anal) {
+		return anal.substring(0, anal.indexOf("["));
+	}
+}
---
+++ src/main/java/hu/ppke/itk/nlpg/purepos/common/Global.java
@@ -0,0 +1,6 @@
+package hu.ppke.itk.nlpg.purepos.common;
+
+public class Global {
+	public static AnalysisQueue analysisQueue = new AnalysisQueue();
+
+}
