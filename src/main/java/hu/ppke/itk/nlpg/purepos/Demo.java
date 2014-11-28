8a172c83ac4735ca06235186a2d99581afd81fc6
György Orosz <oroszgy@gmail.com>
2014-11-28 12:17:53 +0100

    Demo added.

---
+++ src/main/java/hu/ppke/itk/nlpg/purepos/Demo.java
@@ -0,0 +1,38 @@
+package hu.ppke.itk.nlpg.purepos;
+
+import hu.ppke.itk.nlpg.purepos.cli.configuration.Configuration;
+import hu.ppke.itk.nlpg.purepos.common.serializer.SSerializer;
+import hu.ppke.itk.nlpg.purepos.model.internal.CompiledModel;
+import hu.ppke.itk.nlpg.purepos.model.internal.RawModel;
+import hu.ppke.itk.nlpg.purepos.morphology.IMorphologicalAnalyzer;
+import hu.ppke.itk.nlpg.purepos.morphology.NullAnalyzer;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) throws Exception {
+		String modelPath = args[0];
+		double beamLogTheta = Math.log(1000);
+		double suffixLogTheta = Math.log(10);
+		int maxGuessed = 10;
+		boolean useBeamSearch = false;
+		IMorphologicalAnalyzer analyzer = new NullAnalyzer();
+		
+		RawModel rawmodel = SSerializer.readModel(new File(modelPath));
+		CompiledModel<String, Integer> model = rawmodel
+				.compile(new Configuration());
+		
+		ITagger tagger = new MorphTagger(model, analyzer, beamLogTheta, suffixLogTheta,
+				maxGuessed, useBeamSearch);
+		
+		Scanner inputScanner = new Scanner(System.in);
+		PrintStream taggerOutput = new PrintStream(System.out, true);
+		
+		tagger.tag(inputScanner, taggerOutput);
+
+	}
+
+}
